252ef3385e83f4911592968a1978adfa1df57d61
ihugowi <orushugow@gmail.com>
2025-02-24 12:52:25 -0300

    Criação básica dos dominios de entidades.

---
+++ HelpApp/HelpApp.Domain/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace HelpApp.Domain.Entities
+{
+    public class Product
+    {
+        #region Atributos
+        public int Id { get; set; }    
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+        public int CategoryID { get; set; }
+
+        #endregion
+
+        #region Constructors
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+
+        #endregion
+    }
+}
